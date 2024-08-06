81af923d225aa7bc232d408e0229e9ed766434a1
Comfort Ngwenya <>
2024-08-06 22:19:45 +0200

    fixing search function read from the database

--- todo-list/src/Components/Search/Search.jsx
+++ todo-list/src/Components/Search/Search.jsx
@@ -2,6 +2,7 @@ import './Search.css';
 import React, { useState } from 'react';
 import { TextField, Box, Button, Typography } from '@mui/material';
 import axios from 'axios';
+import { initDatabase, searchTasks } from '../../Database/Statements';
 
 export default function Search({ setTasks }) {
   const [searchQuery, setSearchQuery] = useState('');
@@ -19,28 +20,13 @@ export default function Search({ setTasks }) {
     console.log('Searching for:', searchQuery, 'with userId:', user.id);
 
     try {
-      const response = await axios.get('http://localhost:8888/todos', {
-        params: {
-          // q: searchQuery,
-          userId: user.id
-        }
-      });
-
-
-      if (!Array.isArray(response.data)) {
-        console.error('Unexpected response data:', response.data);
-        setSearchStatus('Error occurred while searching');
-        return;
-      }
-
-
-      const filteredTasks = response.data.filter(task => 
-        task.taskName && task.taskName.toLowerCase().includes(searchQuery.toLowerCase())
-      );
+      
+        const database = await initDatabase();
+        const filteredTasks = searchTasks(database, user.id, searchQuery);
   
       console.log('Filtered tasks:', filteredTasks);
 
-      console.log('Search response:', response.data)
+      // console.log('Search response:', response.data)
 
       if (filteredTasks.length === 0) {
         console.log('no data')
@@ -54,6 +40,7 @@ export default function Search({ setTasks }) {
       console.error('Error searching tasks:', error);
       setSearchStatus('Error occurred while searching');
     }
+    
   };
 
   return (
